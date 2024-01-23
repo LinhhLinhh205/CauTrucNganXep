221d09a6040d8e4fba23649e36d79e4c38b44c05
Ngọc Linh <linhlinhbehy@gmail.com>
2024-01-23 18:12:52 +0700

    Add project files.

---
+++ BaiTapNganXep/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapNganXep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so nguyen N:");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Nhap he so X can doi:");
+            int x = int.Parse(Console.ReadLine());
+
+            Stack s = new Stack();
+            while (n > 0)
+            {
+                s.Push(n % x);
+                n = n / x;
+            }
+            int value;
+            Console.WriteLine($"Bieu dien he so {x}:");
+            while (!s.isEmpty())
+            {
+                value = s.Pop();
+                if (x == 16)
+                {
+                    switch (value)
+                    {
+                        case 10:
+                            Console.Write("A");
+                            break;
+                        case 11:
+                            Console.Write("B");
+                            break;
+                        case 12:
+                            Console.Write("C");
+                            break;
+                        case 13:
+                            Console.Write("D");
+                            break;
+                        default:
+                            Console.Write(value);
+                            break;
+                    }
+                }
+                else Console.Write(value);
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ BaiTapNganXep/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapNganXep
+{
+    class Stack
+    {
+        private int[] data;
+        private int top;
+        public Stack()
+        {
+            data = new int[100];
+            top = -1;
+
+        }
+        public Stack(int size)
+        {
+            data = new int[size];
+            top = -1;
+        }
+        public bool isEmpty()
+        {
+            bool kq = false;
+            if (top == -1)
+                kq = true;
+            return kq;
+        }
+        public bool isFull()
+        {
+            bool kq = false;
+            if (top == data.Length - 1)
+                kq = true;
+            return kq;
+        }
+        public void Push(int x)
+        {
+            if (isFull())
+                Console.WriteLine("Stack da day");
+            else
+            {
+                top++;
+                data[top] = x;
+            }
+        }
+        public int Pop()
+        {
+            int kq = 0;
+            if (isEmpty())
+                Console.WriteLine("Stack da day.Them that bai");
+            else
+            {
+                kq = data[top];
+                top--;
+
+            }
+            return kq;
+        }
+    }
+}
